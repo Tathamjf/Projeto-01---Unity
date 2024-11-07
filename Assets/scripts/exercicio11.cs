11813a5d253d9d595d94366ff6b48686250c4038
Tathamjf <1agatha.m.f@gmail.com>
2024-11-07 15:37:54 -0300

    feat: Exercicio 11

--- Assets/scripts/exercicio11.cs
+++ Assets/scripts/exercicio11.cs
@@ -1,11 +1,39 @@
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class exercicio11 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //(Diálogos) Implemente um sistema de diálogos onde o jogador
+    //pode interagir com um NPC(personagem não jogável), e o NPC
+    //responde com diferentes frases dependendo do estado do jogo.
+    //Use switch case para definir as respostas baseadas no estado do
+    //jogador.
+
+    [SerializeField] int estado;
+
     void Start()
     {
-        
+        switch (estado)
+        {
+            case 0:
+                print("Ola viajante! tudo bem???");
+                break;
+
+            case 1:
+                print("Boa Viagem!");
+                break;
+
+            case 2:
+                print("Acabou, amém!");
+                break;
+
+            default:
+                print("Ferrou :(");
+                break;
+        }
+
+
+
     }
 
     // Update is called once per frame
