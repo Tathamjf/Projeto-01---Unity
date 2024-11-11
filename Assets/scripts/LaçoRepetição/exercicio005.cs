765f5629e94e499bc07856456c3dc73663823cb4
Tathamjf <1agatha.m.f@gmail.com>
2024-11-11 15:09:00 -0300

    feat: Exercicio 05

---
+++ Assets/scripts/LaçoRepetição/exercicio005.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class exercicio005 : MonoBehaviour
+{
+    //5. (Aumento de nível de dificuldade) A cada fase completada, a
+    //dificuldade aumenta em 5 pontos.Exiba a dificuldade após cada
+    //fase, em um total de 6 fases.
+
+    [SerializeField] int dificuldade = 0;
+    void Start()
+    {
+        for (int fase = 0; fase < +6; fase++)
+        {
+            dificuldade += 5;
+            print("Fase: " + fase + "; Dificuldade : " + dificuldade);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
