3a1f41ff6ebc3aecba6294e5ffc435611c832817
Tathamjf <1agatha.m.f@gmail.com>
2024-11-11 15:15:47 -0300

    feat: Exercicio 07

---
+++ Assets/scripts/LaçoRepetição/exercicio007.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using UnityEngine;
+using static Unity.Burst.Intrinsics.X86;
+
+public class Exercício07 : MonoBehaviour
+{
+    //7. (Maior número em uma lista) Dada uma lista de números,
+    //encontre o maior valor.
+    void Start()
+    {
+        List<int> n = new List<int>();
+        n.Add(1);
+        n.Add(15);
+        n.Add(100);
+        n.Add(34);
+        n.Add(235);
+        n.Add(220);
+        n.Add(126);
+
+        int num = n.Max();
+        print("O maior número dessa lista é o: " + num);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
