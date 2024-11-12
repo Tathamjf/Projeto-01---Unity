055151649524ad62514ae3c8f90534a2a837a339
Tathamjf <1agatha.m.f@gmail.com>
2024-11-12 17:09:55 -0300

    feat: Correção Exercicio 07

--- Assets/scripts/LaçoRepetição/exercicio007.cs
+++ Assets/scripts/LaçoRepetição/exercicio007.cs
@@ -8,19 +8,37 @@ public class Exercício007 : MonoBehaviour
 {
     //7. (Maior número em uma lista) Dada uma lista de números,
     //encontre o maior valor.
+
+    int[] numeros = { 3, 5, 45, 7, 9, 8, 20, 47 };
+    int maior = 0;
+
     void Start()
     {
-        List<int> n = new List<int>();
-        n.Add(1);
-        n.Add(15);
-        n.Add(100);
-        n.Add(34);
-        n.Add(235);
-        n.Add(220);
-        n.Add(126);
+        //List<int> n = new List<int>();
+        //n.Add(1);
+        //n.Add(15);
+        //n.Add(100);
+        //n.Add(34);
+        //n.Add(235);
+        //n.Add(220);
+        //n.Add(126);
 
-        int num = n.Max();
-        print("O maior número dessa lista é o: " + num);
+        //int num = n.Max();
+        //print("O maior número dessa lista é o: " + num);
+
+        maior = numeros[0];
+
+        foreach (var item in numeros)
+        {
+            if (item > maior)
+            {
+                maior = item;
+            }
+        }
+
+        print("O maior numero é: " + maior);
+        
+
 
 
     }
